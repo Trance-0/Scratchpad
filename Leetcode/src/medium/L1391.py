aeef931b5fe6097ff345fde5e43be4dc798716c1
Zheyuan Wu <60459821+Trance-0@users.noreply.github.com>
2026-04-26 21:07:48 -0500

    Update L1391.py

--- Leetcode/src/medium/L1391.py
+++ Leetcode/src/medium/L1391.py
@@ -4,17 +4,19 @@ class Solution:
         m,n=len(grid),len(grid[0])
         visited=[[False]*n for _ in range(m)]
         def dfs(px,py,x,y):
-            print(x,y)
+            # print(px,py,x,y)
             if x==m-1 and y==n-1:
                 return True
             if visited[x][y]:
                 return False
             visited[x][y]=True
             dx,dy=street[grid[x][y]-1][0]
-            if x+dx!=px and y+dy!=py and 0<=x+dx<m and 0<=y+dy<n and (dx,dy) in street[grid[x+dx][y+dy]-1] and dfs(x,y,x+dx,y+dy):
+            # print(dx,dy)
+            if (x+dx!=px or y+dy!=py) and 0<=x+dx<m and 0<=y+dy<n and (-dx,-dy) in street[grid[x+dx][y+dy]-1] and dfs(x,y,x+dx,y+dy):
                 return True
             dx,dy=street[grid[x][y]-1][1]
-            if  x+dx!=px and y+dy!=py and 0<=x+dx<m and 0<=y+dy<n and (dx,dy) in street[grid[x+dx][y+dy]-1]  and dfs(x,y,x+dx,y+dy):
+            # print(dx,dy, (0,-1) in ((0,-1),(0,1)))
+            if  (x+dx!=px or y+dy!=py) and 0<=x+dx<m and 0<=y+dy<n and (-dx,-dy) in street[grid[x+dx][y+dy]-1]  and dfs(x,y,x+dx,y+dy):
                 return True
             return False
         return dfs(-1,-1,0,0)
