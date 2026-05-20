19fc8d07b91ca4ed848e26c4d9d045ae6ffa7c4a
Trance-0 <60459821+Trance-0@users.noreply.github.com>
2026-05-19 20:02:34 -0400

    Create L154.cpp

---
+++ Leetcode/src/hard/L154.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int lo=0;
+        for (int hi=1;hi<nums.size();hi++){
+            if (nums[lo]==nums[hi]){
+                continue;
+            }
+            lo+=1;
+            nums[lo]=nums[hi];
+        }
+        nums=nums.substr(lo,nums.size()-lo);
+        // the array is partially sorted, thus, binary search based on high element..
+        int low=0,high=lo;
+        while (low<high){
+            int mid=low+(high-low)/2;
+            // if middle element is lower than high element, the start of array is before mid.
+            if(nums[mid]<nums[high]){
+                high=mid;
+            }else{
+                low=mid+1;
+            }
+        }
+        return nums[low];
+    }
+};
